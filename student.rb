1f1d7cd5c95b811db06b2d7ce6803bd206f697fe
Anwar <anwarhussaini160@gmail.com>
2023-08-28 22:51:22 +0430

    Add student class

---
+++ student.rb
@@ -0,0 +1,13 @@
+require_relative 'person'
+class Student < Person
+  attr_accessor :classroom
+
+  def initialize(id, age, classroom, parent_permission: true, name: 'Unknown')
+    super(id, age, parent_permission, name)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+end
