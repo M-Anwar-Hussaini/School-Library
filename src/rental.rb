b35bf8d0eec0b1069950fe45bc47dc3b8bf97ebd
Anwar <anwarhussaini160@gmail.com>
2023-08-31 03:18:23 +0430

    Customize the constructor of rentals

--- src/rental.rb
+++ src/rental.rb
@@ -4,6 +4,9 @@ class Rental
   attr_accessor :date, :book, :person
 
   def initialize(date, book, person)
+    book.rentals << self unless book.rentals.include?(self)
+    person.rentals << self unless person.rentals.include?(self)
+
     @date = date
     @book = book
     @person = person
