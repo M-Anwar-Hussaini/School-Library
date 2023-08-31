ecdddcfa6886a698093989c5551bbb2e8a9a0097
Anwar <anwarhussaini160@gmail.com>
2023-08-31 21:33:04 +0430

    Override to_s method of Student class

--- src/student.rb
+++ src/student.rb
@@ -3,7 +3,7 @@ class Student < Person
   attr_reader :classroom
 
   def initialize(age, classroom, parent_permission: true, name: 'Unknown')
-    super(age, parent_permission, name)
+    super(age, name)
     @classroom = classroom
   end
 
@@ -14,5 +14,9 @@ class Student < Person
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def to_s
+    "[Student] Name: #{name}, ID: #{id}, Age: #{age}"
   end
 end
